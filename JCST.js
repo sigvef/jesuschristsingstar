80fbd182f117c875e27654d8541dd2929757fee4
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-11-23 06:47:25 +0100

    Restructuring JCST

--- JCST.js
+++ JCST.js
@@ -6,11 +6,13 @@ function JCST(){
     this.date = new Date();
     this.oldDate = this.date;
     this.dt = 0;
-    this.canvas = document.createElement('canvas');
+    this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
-    this.canvas.width = 32000; 
-    this.canvas.height = 600;
     this.segment_width = 1;
+    this.notes = [];
+    this.cached_note_start = 0;
+    this.pixels_per_second = 400;
+    this.pixels_per_semitone = 20;
 }
 
 
@@ -22,50 +24,78 @@ JCST.prototype.loadMidi = function(name, cb){
 
         that.reset();
         that.song = data;
-        that.events = that.song.midi.tracks[data.voice_track].events;
-        that.render(); 
+
+        var events = that.song.midi.tracks[data.voice_track].events;
+
+        var note_start = 0;
+
+        var time = 0;
+
+        for(var i=0;i<events.length;i++){
+            var event = events[i];
+            time += event.dt / that.song.midi.ticks_per_second;
+
+            if(event.type == 9){
+                note_start = time; 
+            }
+            else if(event.type == 8){
+                that.notes.push({start:note_start, length: time-note_start, note_number:event.note_number});
+            }
+        }
+
+        console.log("ladedd");
+        that.start();
+
    });
 }
+
+JCST.prototype.start = function(){
+    
+    var that = this;
+    setInterval(function(){that.render()},10);
+
+};
 
 JCST.prototype.reset = function(){
     clearInterval(this.interval);
     this.song_progress = -1;
     this.dt_progress = 0;
+    this.notes = [];
 };
+
+
+JCST.prototype.getNotesBetween = function(from, to){
+
+    var notes = [];
+
+    var i = this.cached_note_start;
+
+    while(this.notes[i].start <= from){
+        i++;
+    }
+
+
+    while(this.notes[i].start < to){
+        notes.push(this.notes[i]);
+        i++;
+    }   
+
+    return notes;
+};
+
+
 
 JCST.prototype.render = function(){
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 
-        
-    var that = this;
+    var notes = this.getNotesBetween(0,10);
 
-    this.segment_width = 1;
-    var segment_height = 20;
-
-    while(this.currentEventIndex < this.events.length-1){
-        that.play_forward();
-
-        if(that.events[that.currentEventIndex].type == 9){
-        that.ctx.fillRect(this.song_progress*this.segment_width, segment_height*(90-that.events[that.currentEventIndex].note_number), this.segment_width, segment_height);
-}
+    console.log(notes.length);
+    for(var i=0;i<notes.length;i++){
+        var note = notes[i];
+        this.ctx.fillRect(note.start*this.pixels_per_second, (80-note.note_number)*this.pixels_per_semitone, note.length*this.pixels_per_second, 1*this.pixels_per_semitone);
     }
-
-    document.body.appendChild(this.canvas); 
-
 };
 
-
-
-JCST.prototype.play_forward = function(){
-
-    this.dt_progress--;
-
-    if(this.dt_progress < 0){
-        this.currentEventIndex++;
-        console.log("next event");
-        this.dt_progress = (this.events[this.currentEventIndex] || {dt:0}).dt;
-    }
-
-    this.song_progress++;
-};
 
 JesusChristSingStar = JCST;
