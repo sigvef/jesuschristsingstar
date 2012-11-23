201198982198b48c284d5bcf5c328963779d74e2
Stian Jensen <me@stianj.com>
2012-11-23 06:01:41 +0100

    song picker first draft

--- main.js
+++ main.js
@@ -2,10 +2,20 @@
     $(document).ready(function(){
         document.getElementById("start-button").addEventListener("click", startGame);
     });
+    
+    var l = new LyricsMachine();
+    var lTimer;
 
     function startGame() {
+        l.updateLines(8);
+        l.updateLines(8);
         $('#splash-container').fadeOut("fast");
-        $('#game-container').show();
+        $('#game-container').fadeIn("fast");
+        var song = document.getElementById("song-picker").value;
+        lTimer = setInterval(function(){l.updateLines(8)}, 5000);
+    }
+    function stopGame() {
+        clearInterval(lTimer);
     }
 })();
 
